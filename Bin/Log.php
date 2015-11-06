993c9fc27f49c5942641ef60494355e23d08673b
fitzzzz <david.lang697@gmail.com>
2015-11-06 18:27:13 +0100

    ptut version js

--- Bin/Log.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$host = "iutdoua-webetu.univ-lyon1.fr"; $user = "p1317719"; 
-	$password = "201912"; 
-	$nombase = "p1317719";
-?>
--- Model/Ptut/ptuterror.php
+++
@@ -1,7 +0,0 @@
-<?php $title = 'Erreur';
-ob_start();
-
-echo '<p class="err">Une erreur est survenue : ' . $msg . '</p>';
-
-$content = ob_get_clean();
-require 'ptutlayout.php';
